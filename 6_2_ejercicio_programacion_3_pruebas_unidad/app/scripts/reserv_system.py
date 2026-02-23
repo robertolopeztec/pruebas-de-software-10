decb4d2f6b72c218485d3919c0ed8e0a044a7e4d
Roberto Lopez <unk>
2026-02-22 19:31:27 -0600

    Replaced hotels-catalogue format from .csv to j.son

--- 6_2_ejercicio_programacion_3_pruebas_unidad/app/scripts/reserv_system.py
+++ 6_2_ejercicio_programacion_3_pruebas_unidad/app/scripts/reserv_system.py
@@ -1,7 +1,11 @@
 """
 This file contains the core objects for the reservation system.
+> Assumes that the core script is run from the root path.
 """
 import os
+import json
+
+CATALOGUE_DIR = 'app/data/'
 
 
 def read_content_from_file(file: str):
@@ -10,21 +14,42 @@ def read_content_from_file(file: str):
     """
 
     with open(file, 'r', encoding='utf8') as f:
-        content = f.readlines()
+        content = json.load(f)
         f.close()
     return content
 
 
-class Hotel(object):
-    def create_hotel(self, hotel_id: str, hotel_name: str):
+class Hotel():
+    """
+    The core Hotel class
+    """
+    def __init__(self):
+        self.hotel_file = os.path.join(CATALOGUE_DIR, 'hotels.json')
+    
+    def create_hotel(self, hotel_id: int, hotel_name: str, hotel_rooms: int):
         """
         Generate a new record within the hotel catalogue.
         """
 
+        # Validate if the schema for creating a hotel is valid
+        if (
+            isinstance(hotel_id, int)
+            and isinstance(hotel_name, str)
+            and isinstance(hotel_rooms, int)
+        ):
+            raise TypeError('Invalid type: {}, {}, {}'.format(type(hotel_id),
+                                                              type(hotel_name),
+                                                              type(hotel_rooms)
+                                                             )
+                           )
+
+        # content = read_content_from_file(self.hotel_file)
 
 if __name__ == '__main__':
-    x = read_content_from_file('/data/hotels.csv')
-    
+
+    h = Hotel()
+    print(h.hotel_file)
+    h.create_hotel('s', 's', 2)
 
     
     
